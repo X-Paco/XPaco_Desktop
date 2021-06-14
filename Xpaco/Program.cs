c2f22789471c926928c5f2d0200502861d3beeec
Aquiles Rodrigues <cyberfazer@gmail.com>
2021-06-14 17:29:07 -0300

    Correção do tipo de projeto

--- Xpaco/Program.cs
+++ Xpaco/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,15 +9,14 @@ namespace Xpaco
     static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Ponto de entrada principal para o aplicativo.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
+            Application.Run(new Form1());
         }
     }
 }
