046653424818dd17927b0095ebefdd4e3ed9d85d
Aquiles Rodrigues <cyberfazer@gmail.com>
2021-06-14 18:35:42 -0300

    Criação de  Model

---
+++ Xpaco/models/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xpaco.models
+{
+    public class Profile
+    {
+        public object einItin { get; set; }
+        public object codState { get; set; }
+        public object phone { get; set; }
+        public object num { get; set; }
+        public object address { get; set; }
+        public object district { get; set; }
+        public object city { get; set; }
+        public object state { get; set; }
+        public object zipCode { get; set; }
+        public object photo { get; set; }
+        public object birthday { get; set; }
+    }
+
+}
---
+++ Xpaco/models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xpaco.models
+{
+ 
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string nickname { get; set; }
+        public string email { get; set; }
+        public string mobile { get; set; }
+        public int personaId { get; set; }
+        public Profile profile { get; set; }
+    }
+
+/***    public class Root
+    {
+        public List<User> user { get; set; }
+    }
+*/
+}
