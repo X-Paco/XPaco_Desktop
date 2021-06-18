51416ebb6b5865388c2d7b04f3b83ca4ae31f4c6
Paulo-P-33 <luiz.lira@a.ficr.edu.br>
2021-06-18 17:45:43 -0300

    Material

---
+++ Xpaco/view/Material.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace Xpaco.view
+{
+    partial class Material
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Material));
+            this.panelPaginaMaterial = new System.Windows.Forms.Panel();
+            this.pbLogoPaginaMaterial = new System.Windows.Forms.PictureBox();
+            this.btnFecharPaginaCadastro = new System.Windows.Forms.PictureBox();
+            this.btnMaximizarPaginaCadastro = new System.Windows.Forms.PictureBox();
+            this.btnMinimizarPaginaCadastro = new System.Windows.Forms.PictureBox();
+            this.btnRestaurarTamanhoPaginaCadastro = new System.Windows.Forms.PictureBox();
+            this.panelEsquerdoPaginaMaterial = new System.Windows.Forms.Panel();
+            this.panelBottomPaginaMaterial = new System.Windows.Forms.Panel();
+            this.panelDireitoPaginaMaterial = new System.Windows.Forms.Panel();
+            this.panelPaginaMaterial.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoPaginaMaterial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFecharPaginaCadastro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximizarPaginaCadastro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizarPaginaCadastro)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRestaurarTamanhoPaginaCadastro)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelPaginaMaterial
+            // 
+            this.panelPaginaMaterial.BackColor = System.Drawing.Color.Gray;
+            this.panelPaginaMaterial.Controls.Add(this.pbLogoPaginaMaterial);
+            this.panelPaginaMaterial.Controls.Add(this.btnFecharPaginaCadastro);
+            this.panelPaginaMaterial.Controls.Add(this.btnMaximizarPaginaCadastro);
+            this.panelPaginaMaterial.Controls.Add(this.btnMinimizarPaginaCadastro);
+            this.panelPaginaMaterial.Controls.Add(this.btnRestaurarTamanhoPaginaCadastro);
+            this.panelPaginaMaterial.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelPaginaMaterial.Location = new System.Drawing.Point(0, 0);
+            this.panelPaginaMaterial.Name = "panelPaginaMaterial";
+            this.panelPaginaMaterial.Size = new System.Drawing.Size(800, 62);
+            this.panelPaginaMaterial.TabIndex = 3;
+            // 
+            // pbLogoPaginaMaterial
+            // 
+            this.pbLogoPaginaMaterial.Image = ((System.Drawing.Image)(resources.GetObject("pbLogoPaginaMaterial.Image")));
+            this.pbLogoPaginaMaterial.Location = new System.Drawing.Point(0, 0);
+            this.pbLogoPaginaMaterial.Name = "pbLogoPaginaMaterial";
+            this.pbLogoPaginaMaterial.Size = new System.Drawing.Size(137, 62);
+            this.pbLogoPaginaMaterial.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogoPaginaMaterial.TabIndex = 44;
+            this.pbLogoPaginaMaterial.TabStop = false;
+            // 
+            // btnFecharPaginaCadastro
+            // 
+            this.btnFecharPaginaCadastro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFecharPaginaCadastro.Location = new System.Drawing.Point(769, 23);
+            this.btnFecharPaginaCadastro.Name = "btnFecharPaginaCadastro";
+            this.btnFecharPaginaCadastro.Size = new System.Drawing.Size(0, 0);
+            this.btnFecharPaginaCadastro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnFecharPaginaCadastro.TabIndex = 48;
+            this.btnFecharPaginaCadastro.TabStop = false;
+            // 
+            // btnMaximizarPaginaCadastro
+            // 
+            this.btnMaximizarPaginaCadastro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaximizarPaginaCadastro.Location = new System.Drawing.Point(742, 23);
+            this.btnMaximizarPaginaCadastro.Name = "btnMaximizarPaginaCadastro";
+            this.btnMaximizarPaginaCadastro.Size = new System.Drawing.Size(0, 0);
+            this.btnMaximizarPaginaCadastro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMaximizarPaginaCadastro.TabIndex = 49;
+            this.btnMaximizarPaginaCadastro.TabStop = false;
+            // 
+            // btnMinimizarPaginaCadastro
+            // 
+            this.btnMinimizarPaginaCadastro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizarPaginaCadastro.Location = new System.Drawing.Point(715, 23);
+            this.btnMinimizarPaginaCadastro.Name = "btnMinimizarPaginaCadastro";
+            this.btnMinimizarPaginaCadastro.Size = new System.Drawing.Size(0, 0);
+            this.btnMinimizarPaginaCadastro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnMinimizarPaginaCadastro.TabIndex = 50;
+            this.btnMinimizarPaginaCadastro.TabStop = false;
+            // 
+            // btnRestaurarTamanhoPaginaCadastro
+            // 
+            this.btnRestaurarTamanhoPaginaCadastro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRestaurarTamanhoPaginaCadastro.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRestaurarTamanhoPaginaCadastro.Location = new System.Drawing.Point(742, 23);
+            this.btnRestaurarTamanhoPaginaCadastro.Name = "btnRestaurarTamanhoPaginaCadastro";
+            this.btnRestaurarTamanhoPaginaCadastro.Size = new System.Drawing.Size(0, 0);
+            this.btnRestaurarTamanhoPaginaCadastro.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.btnRestaurarTamanhoPaginaCadastro.TabIndex = 47;
+            this.btnRestaurarTamanhoPaginaCadastro.TabStop = false;
+            this.btnRestaurarTamanhoPaginaCadastro.Visible = false;
+            // 
+            // panelEsquerdoPaginaMaterial
+            // 
+            this.panelEsquerdoPaginaMaterial.BackColor = System.Drawing.Color.Silver;
+            this.panelEsquerdoPaginaMaterial.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelEsquerdoPaginaMaterial.Location = new System.Drawing.Point(0, 62);
+            this.panelEsquerdoPaginaMaterial.Name = "panelEsquerdoPaginaMaterial";
+            this.panelEsquerdoPaginaMaterial.Size = new System.Drawing.Size(171, 301);
+            this.panelEsquerdoPaginaMaterial.TabIndex = 47;
+            // 
+            // panelBottomPaginaMaterial
+            // 
+            this.panelBottomPaginaMaterial.BackColor = System.Drawing.Color.Silver;
+            this.panelBottomPaginaMaterial.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottomPaginaMaterial.Location = new System.Drawing.Point(0, 363);
+            this.panelBottomPaginaMaterial.Name = "panelBottomPaginaMaterial";
+            this.panelBottomPaginaMaterial.Size = new System.Drawing.Size(800, 87);
+            this.panelBottomPaginaMaterial.TabIndex = 48;
+            this.panelBottomPaginaMaterial.Visible = false;
+            // 
+            // panelDireitoPaginaMaterial
+            // 
+            this.panelDireitoPaginaMaterial.BackColor = System.Drawing.Color.Silver;
+            this.panelDireitoPaginaMaterial.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelDireitoPaginaMaterial.Location = new System.Drawing.Point(629, 62);
+            this.panelDireitoPaginaMaterial.Name = "panelDireitoPaginaMaterial";
+            this.panelDireitoPaginaMaterial.Size = new System.Drawing.Size(171, 301);
+            this.panelDireitoPaginaMaterial.TabIndex = 49;
+            // 
+            // Material
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelDireitoPaginaMaterial);
+            this.Controls.Add(this.panelEsquerdoPaginaMaterial);
+            this.Controls.Add(this.panelPaginaMaterial);
+            this.Controls.Add(this.panelBottomPaginaMaterial);
+            this.Name = "Material";
+            this.Text = "Material";
+            this.panelPaginaMaterial.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoPaginaMaterial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFecharPaginaCadastro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMaximizarPaginaCadastro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizarPaginaCadastro)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnRestaurarTamanhoPaginaCadastro)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelPaginaMaterial;
+        private System.Windows.Forms.PictureBox pbLogoPaginaMaterial;
+        private System.Windows.Forms.PictureBox btnFecharPaginaCadastro;
+        private System.Windows.Forms.PictureBox btnMaximizarPaginaCadastro;
+        private System.Windows.Forms.PictureBox btnMinimizarPaginaCadastro;
+        private System.Windows.Forms.PictureBox btnRestaurarTamanhoPaginaCadastro;
+        private System.Windows.Forms.Panel panelEsquerdoPaginaMaterial;
+        private System.Windows.Forms.Panel panelBottomPaginaMaterial;
+        private System.Windows.Forms.Panel panelDireitoPaginaMaterial;
+    }
+}
